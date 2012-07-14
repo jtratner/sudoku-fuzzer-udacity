887b2b561f625d1d629c6b029764191c29120901
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2012-07-13 22:19:09 -0400

    fuzz_solver using transpose, reflection, etc
    from kedarbellare's gist here: https://gist.github.com/3108759

--- fuzz_solver.py
+++ fuzz_solver.py
@@ -99,6 +99,48 @@ def check_random_solns(solve_sudoku, check_sudoku, mutates, iters):
     print "Your solver completed %d / %d random boards! Congrats!" % (solved, iters)
     return True        
 
+# Random strategy 2: Take a valid board and perform transformations
+# that do not change validity
+
+# Transposing a grid maintains validity
+def transpose(grid):
+    return map(list, zip(*grid))
+
+# Permutes the row/column with another row/column in the same range
+# (i.e. 6 with 6-8, 0 with 0-2, etc.)
+def permute(grid, i, row=True):
+    if not row: grid = map(list, zip(*grid))
+    j = random.choice(range(i/3*3, i/3*3+3))
+    grid[j], grid[i] = grid[i], grid[j]
+    return grid if row else transpose(grid)
+
+# Permutes the row/column blocks (i.e. 0-2 with 6-8)
+def permute_block(grid, i, row=True):
+    if not row: grid = map(list, zip(*grid))
+    bi = i*3
+    bj = random.choice(range(3))*3
+    for offset in range(3):
+        grid[bi+offset], grid[bj+offset] = grid[bj+offset], grid[bi+offset]
+    return grid if row else transpose(grid)
+
+# Reflects the board along the horizontal or vertical axis
+def reflect(grid, horizontal=True):
+    if not horizontal: grid = map(list, zip(*grid))
+    for i in range(9): grid[i].reverse()
+    return grid if horizontal else transpose(grid)
+
+def random_mutation_sudoku(soln, iters=1000):
+    # generate a valid grid
+    grid = copy(soln)
+    choices = [['reflect', horizontal] for horizontal in (True, False)] + [['transpose']] + [['permute', i, row] for row in (True, False) for i in range(9)] + [['permute_block', bi, row] for row in (True, False) for bi in range(3)]
+    for i in range(iters):
+        choice = random.choice(choices)
+        if choice[0] == 'reflect': grid = reflect(grid, *choice[1:])
+        if choice[0] == 'transpose': grid = transpose(grid)
+        if choice[0] == 'permute': grid = permute(grid, *choice[1:])
+        if choice[0] == 'permute_block': grid = permute_block(grid, *choice[1:])
+    return grid
+
 # Make a copy of a grid so we can modify it without touching the original
 def copy (grid):
     return map (lambda x: x[:], grid)
@@ -134,4 +176,9 @@ Returned {res} instead. Input was: {test}""".format(test=test, res=res)
 
 def fuzz_solver(check_sudoku, solve_sudoku, mutates=10, iters=10, soln=None, tests=None):
     soln = soln or valid
-    return check_no_valid_solns(solve_sudoku, tests) and fuzz_solution(valid, mutates, iters, check_sudoku, solve_sudoku) and check_random_solns(solve_sudoku, check_sudoku, mutates, iters)
+    success = check_no_valid_solns(solve_sudoku, tests) and fuzz_solution(soln, mutates, iters, check_sudoku, solve_sudoku) and check_random_solns(solve_sudoku, check_sudoku, mutates, iters)
+    if success and all(fuzz_solution(random_mutation_sudoku(soln), mutates, 1, check_sudoku, solve_sudoku) for _ in range(iters)):
+        print "Your solver completed %d randomly generated boards with %d mutations! Congrats!" % (iters, mutates)
+        return True
+    else:
+        return False
